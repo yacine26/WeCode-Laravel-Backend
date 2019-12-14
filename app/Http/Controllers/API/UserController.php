0595da2c9096b32ed9484814079bf00bd2286838
AmineCh1997 <yacine.mazouz@esprit.tn>
2019-12-14 03:11:44 +0100

    Lravel cors added

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -17,7 +17,7 @@ class UserController extends Controller
         if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
             $user = Auth::user();
             $success['token'] =  $user->createToken('MyApp')-> accessToken;
-            return response()->json(['success' => $success], $this-> successStatus);
+            return response()->json(['success' => $user], $this-> successStatus);
         }
         else{
             return response()->json(['error'=>'Unauthorised'], 401);
